2be4ba0d6714079480d60ff352614fdb7fd642d0
tTimeus <littlemonkies29@gmail.com>
2025-11-03 16:39:16 +1000

    Added some simple JS
    - Copy button text changes and cannot be clicked for 2 seconds
    - Added eventListeners for both buttons and it works

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 /* JavaScript Component */
 /* Event listeners */
+document.getElementById('inspect_btn').addEventListener('click', inspectPage);
+document.getElementById('copy_btn').addEventListener('click', copiedPage);
+
+/* Inspects the page and gives the template */
+function inspectPage() {
+    const template = document.getElementById('template');
+    template.innerHTML = "Page inspected!";
+}
+
+/* Copies the content from the template onto clipboard */
+function copiedPage() {
+    const copy = document.getElementById('copy_btn');
+
+    // Change text to "Copied!"
+    copy.textContent = "Copied!";
+
+    // Disable button temporarily
+    copy.disabled = true;
+
+    // Changes back after 2 seconds
+    setTimeout(() => {
+    copy.textContent = "Copy";
+    copy.disabled = false;
+    }, 2000);
+}
